a8a0be499a92d51e60ffc6da34ff68bdaf21138e
Sasha Derkachev <derkachevsasha2145@gmail.com>
2020-04-05 18:07:37 +0300

    First_commit

---
+++ Task1/index.js
@@ -0,0 +1,109 @@
+'use strict';
+
+/*
+let oneWidthContainer = 12;
+let oneNameContainer = "Sasha and to yoy";
+let oneHeight = true;
+console.log(4/0);
+console.log(oneWidthContainer);
+console.log("string"*7);
+let persom = {
+    name: "Sasha",
+    age: 12,
+    isWarried: false,
+    nimbercar: 10
+    
+}
+console.log("Name:", persom.name,"Age:", persom.age, "Warried:", persom.isWarried,"Numbercar:", persom.nimbercar);
+let arr = ["one", "two", "there"];
+console.log(arr[0],arr[1],arr[2]);
+
+
+
+let answer = +prompt("Вам есть 18?", "Да");
+console.log(typeof(+prompt));  */
+/*
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budjec: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income : [],
+    savings: false,
+      
+};
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+    appData.expenses[a1] = a2;
+    appData.expenses[a3] = a4;
+
+alert(appData.budjec/30);
+console.log(appData.expenses[a1]);
+console.log(appData.expenses[a3]);
+console.log(appData.expenses);
+
+
+let num = 50;
+if (num <49){
+    console.log("Неверно")
+} else if (num > 100){
+    console.log("Много")
+} else {
+    console.log("Верно")
+};
+
+(num == 50) ? console.log("Верно") : console.log("Неверно");
+
+switch (num) {
+    case num < 49:
+         console.log("Неверно");
+         break;
+    case num > 100:
+        console.log("Много");
+        break;
+    case num > 80:
+        console.log("Все ещё много");
+        break;
+    case  50:
+        console.log("Верно");
+        break;
+    default:
+        console.log("Что-то пошло не так");
+        break;
+}   
+
+
+ let a = 10,
+     b = 20;
+
+switch (a){
+    case a > 20: 
+    console.log("No");
+    break;
+    case a < 20:
+    console.log("Yes");
+    break;
+    case 10:
+    console.log("Верно");
+    break;
+} */
+/*
+for ( let i = 1; i < 8; i++ ){
+    if (i ==  6){
+        break; //continue
+    }
+    console.log(i);
+*/
